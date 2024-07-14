4b6272d5b7dc68d1bd3dab9d08c0c1482b29465c
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2024-07-14 18:58:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,92 @@ function Average(A,B,C){
     return (A+B+C)/3;
 }
 console.log(Sum(50, 20,100) + " "+Average(50,20,100) )
+
+//javaScript decision making
+
+//fing Grades
+function findGrade(M){
+    let grade;
+    switch(true){
+        case  M<=10:
+            grade='E'
+            break;
+            case M>=11 && M<=20:
+                grade='D'
+            break;
+            case M>=21 && M<=30:
+                grade='C'
+            break;
+            case M>=31 && M<=40:
+                grade='B'
+            break;
+            case M>=41 && M<=50:
+                grade='A'
+            break;
+            default:
+                grade='Invalid Marks'
+    }
+    console.log(grade);
+}
+findGrade(1);
+//Get Value
+function getValue(character){
+    const Lowerchar = character.toLowerCase();
+    let value;
+    switch(Lowerchar){
+        case 'p':
+            value = "PrepBytes";
+            break;
+            case 'Z':
+                value = 'Zenith';
+                break;
+                case 'e':
+                    value ='Expert Coder';
+                    break;
+                    case 'd':
+                        value = 'Dta Structure';
+                        break;
+                        default:
+                            value = 'Invalid Character'
+    }
+    return value;
+}
+console.log(getValue('E'));
+//find the maxim_um out of three numbers
+function Max_out_of_three(A,B,C){ 
+    if(A === B&& A == C){
+        return -1;
+    }
+    if(A>B&& A>C){
+        return A;
+    }else if(B>A && B>C){
+        return B;
+    }else{
+        return C;
+    }
+}
+console.log(Max_out_of_three(2,5,4))
+
+//second smallest number
+function findSecondSmallest(X,Y,Z){
+   if(X>Y && X>Z){
+     if(Y>Z){
+       return Y;
+     } else{
+       return Z;
+     } 
+   }else if(Y>X&&Y>Z){
+     if(X>Z){
+       return X;
+     } else{
+       return Z;
+     }
+   }else{
+     if(X>Y){
+       return X;
+     } else{
+       return Y;
+     }  }
+   }
+
+console.log(findSecondSmallest(2,9,23))
